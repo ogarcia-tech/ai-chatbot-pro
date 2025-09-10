bd679c070520f246ff43a84e201ce2787a3b969b
ogarcia-tech <ogarcia@metricaweb.es>
2025-09-10 10:39:21 +0200

    Add assistant templates selector and prompt generation

--- ai-chatbot-pro/templates/templates.js
+++ ai-chatbot-pro/templates/templates.js
@@ -1,11 +1,28 @@
 "use strict";
-var ASSISTANT_TEMPLATES = {
-    greeting: '<p>Hola, {{name}}!</p>'
-};
-function renderTemplate(name, data) {
+var ASSISTANT_TEMPLATES = [];
+function loadAssistantTemplates(url) {
+    return fetch(url)
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        ASSISTANT_TEMPLATES = data;
+        window.ASSISTANT_TEMPLATES = ASSISTANT_TEMPLATES;
+        return ASSISTANT_TEMPLATES;
+    });
+}
+function renderTemplate(template, data) {
     if (data === void 0) { data = {}; }
-    var template = ASSISTANT_TEMPLATES[name] || '';
-    return template.replace(/{{\s*(\w+)\s*}}/g, function (_, key) { var _a; return String((_a = data[key]) !== null && _a !== void 0 ? _a : ''); });
+    return template.replace(/{{\s*([\w\.]+)\s*}}/g, function (_, key) {
+        var parts = key.split('.');
+        var value = data;
+        for (var _i = 0, parts_1 = parts; _i < parts_1.length; _i++) {
+            var part = parts_1[_i];
+            value = value === null || value === void 0 ? void 0 : value[part];
+            if (value === undefined || value === null)
+                return '';
+        }
+        return String(value);
+    });
 }
 window.ASSISTANT_TEMPLATES = ASSISTANT_TEMPLATES;
+window.loadAssistantTemplates = loadAssistantTemplates;
 window.renderTemplate = renderTemplate;
